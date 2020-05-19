4aa40ea6e57c933f9cfd025269d8b5b5eafbd6d9
Sandy Maracallo <smaracallo.11@outlook.com>
2020-05-18 23:28:32 -0400

    completed fact & dimension tables

--- sql_queries.py
+++ sql_queries.py
@@ -9,11 +9,25 @@ time_table_drop = "DROP table if exists time"
 # CREATE TABLES
 
 songplay_table_create = (""" 
-CREATE TABLE IF NOT EXISTS songplays (songplay_id int PRIMARY KEY, start_time time, user_id int, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar);
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id SERIAL PRIMARY KEY, 
+    start_time bigint, 
+    user_id int, 
+    level varchar, 
+    song_id varchar, 
+    artist_id varchar, 
+    session_id int, 
+    location varchar, 
+    user_agent TEXT);
 """)
 
 user_table_create = ("""
-CREATE TABLE IF NOT EXISTS users (user_id int PRIMARY KEY, first_name varchar, last_name varchar, gender varchar, level varchar);
+CREATE TABLE IF NOT EXISTS users (
+    user_id int PRIMARY KEY, 
+    first_name varchar, 
+    last_name varchar, 
+    gender varchar, 
+    level varchar);
 """)
 
 song_table_create = ("""
@@ -22,7 +36,7 @@ CREATE TABLE IF NOT EXISTS songs (
     title varchar,
     artist_id varchar,
     year int,
-    duration real)
+    duration real);
 """)
 
 artist_table_create = ("""
@@ -48,9 +62,28 @@ CREATE TABLE IF NOT EXISTS time (
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+INSERT INTO songplays (
+    start_time, 
+    user_id, 
+    level, 
+    song_id, 
+    artist_id, 
+    session_id, 
+    location, 
+    user_agent)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING;
 """)
 
 user_table_insert = ("""
+INSERT INTO users (
+    user_id, 
+    first_name, 
+    last_name, 
+    gender, 
+    level)
+    VALUES (%s, %s, %s, %s, %s) 
+    ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level;
 """)
 
 song_table_insert = ("""
@@ -61,7 +94,7 @@ INSERT INTO songs (
     year,
     duration)
     VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT DO NOTHING
+    ON CONFLICT DO NOTHING;
 """)
 
 artist_table_insert = ("""
@@ -92,6 +125,13 @@ INSERT INTO time (
 # FIND SONGS
 
 song_select = ("""
+    SELECT songs.song_id, artists.artist_id
+    FROM songs 
+    JOIN artists
+    ON songs.artist_id = artists.artist_id
+    WHERE songs.title = %s 
+    AND artists.name = %s 
+    AND songs.duration = %s;
 """)
 
 # QUERY LISTS
